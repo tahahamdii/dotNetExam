24e184eafad1c8bd1dbd4b2b9dcd9d6575237455
Hamdi Taha <hamdi.taha@esprit.tn>
2024-05-13 23:52:06 +0200

    PARTIE II

---
+++ App.ApplicationCore/Services/ElectionService.cs
@@ -0,0 +1,33 @@
+﻿using App.ApplicationCore.Domain;
+using App.ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.ApplicationCore.Services
+{
+    public class ElectionService : Service<Election>, IElectionService
+    {
+        public ElectionService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+
+        public int GetElecteursCount(DateTime dateElection)
+        {
+            return GetById(dateElection).Electeurs.Count;
+        }
+
+        public float GetElecteursJeunesPercent(DateTime dateElection)
+        {
+            var electeursJeunesCount = GetById(dateElection).Electeurs.Where(e =>
+            {
+                var age = (DateTime.Now - e.DateNaissance).TotalDays / 365;
+                return age >= 18 && age <= 35;
+            }).Count();
+            return (float)electeursJeunesCount / GetElecteursCount(dateElection) * 100;
+        }
+    }
+}
---
+++ App.Infrastructure/Migrations/20240513211712_BureauVotee.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Infrastructure.Migrations
+{
+    public partial class BureauVotee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
