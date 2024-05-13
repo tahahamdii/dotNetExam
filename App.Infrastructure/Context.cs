d2666351fd3c7082ed7401d3d7868451421a8ca4
Hamdi Taha <hamdi.taha@esprit.tn>
2024-05-13 22:28:14 +0200

    Entites

---
+++ App.Infrastructure/Context.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infrastructure
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.
+                UseLazyLoadingProxies().
+                UseSqlServer(this.GetJsonConnectionString("appsettings.json"));
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
