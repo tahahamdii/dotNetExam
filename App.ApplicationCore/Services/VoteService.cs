30cc9d1f449321e031e2690dee181a7e9ab5a55a
Hamdi Taha <hamdi.taha@esprit.tn>
2024-05-14 22:33:22 +0200

    CREATE ELECTEUR MODEL VIEW CONTROLLER

--- App.ApplicationCore/Services/VoteService.cs
+++ App.ApplicationCore/Services/VoteService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using App.ApplicationCore.Domain;
+using App.ApplicationCore.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace App.ApplicationCore.Services
 {
-    internal class VoteService
+    public class VoteService: Service<Vote>, IVoteService
     {
+        public VoteService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+
+        public PartiePolitique GetPartiePoitiqueElu(DateTime dateElection)
+        {
+            var partiePolitiqueEluId = GetMany(e => e.DateElection == dateElection).
+                GroupBy(e => e.PartiePolitiqueId).OrderByDescending(e => e.Count()).First().Key;
+
+            return UnitOfWork.Repository<PartiePolitique>().GetById(partiePolitiqueEluId);
+        }
     }
 }
---
+++ App.UI.Web/Controllers/ElecteurController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.UI.Web.Controllers
+{
+    public class ElecteurController : Controller
+    {
+        // GET: ElecteurController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ElecteurController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ElecteurController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ElecteurController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ElecteurController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ElecteurController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ElecteurController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ElecteurController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
